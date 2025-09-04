2f11a83772ac89c4c5e5d03e0eeadd303d45c402
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-09-04 17:23:40 +0200

    feat: ajout de la fonction fit dans Model

--- mlkit/utils/model.py
+++ mlkit/utils/model.py
@@ -1,4 +1,17 @@
 import numpy as np
+from typing import Union
 
 class Model:
-    pass
+    """Parent Object that prepare basic functions"""
+    
+    def fit(self, x:Union[list, np.ndarray], y:Union[list, np.ndarray]):
+        """
+        Train the model to the given data
+        
+        Parameters
+        ----------
+        x:list or numpy.ndarray
+        y:list or numpy.ndarray
+        """
+        
+        pass
