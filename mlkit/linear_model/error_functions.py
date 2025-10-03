e42e1591d47ce5a89647c2c7073193f35e929d2d
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-10-03 17:19:48 +0200

    feat: fonction d'affichage des fonctions d'erreurs

--- mlkit/linear_model/error_functions.py
+++ mlkit/linear_model/error_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class ErrorFunction:
     def __init__(self):
@@ -7,6 +8,9 @@ class ErrorFunction:
     def compute(self, y_true, y_pred):
         raise NotImplementedError("Subclasses should implement this method.")
     
+    def derivative(self, y_true, y_pred):
+        raise NotImplementedError("Subclasses should implement this method.")
+
 class MeanSquaredError(ErrorFunction):
     def compute(self, y_true:np.ndarray, y_pred:np.ndarray) -> float:
         return ((y_true - y_pred) ** 2).mean()
@@ -14,3 +18,25 @@ class MeanSquaredError(ErrorFunction):
 class MeanAbsoluteError(ErrorFunction):
     def compute(self, y_true:np.ndarray, y_pred:np.ndarray) -> float:
         return np.abs(y_true - y_pred).mean()
+
+def plot_function(error_function:ErrorFunction):
+    """Plot the function in blue and its derivative in red"""
+    x = np.linspace(1, 100)
+
+    plt.plot(x, color='blue', label='Function')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('')
+    plt.legend()
+    plt.show()
+    plt.savefig("figures/error_function.png")
+
+if __name__ == "__main__":
+    a = np.array([1])
+    print(a)
+
+    b = np.array([0])
+
+    print(MeanSquaredError().compute(a, b))
+
+    plot_function(MeanAbsoluteError)
--- mlkit/maths/derivative.py
+++ mlkit/maths/derivative.py
@@ -1,12 +1,17 @@
 import inspect
 from collections.abc import Callable
 
+def derive(fn:str) -> str:
+    pass
+
 def get_calc(f:Callable[[float,], float]) -> Callable[[float,], float]:
     source_code = inspect.getsource(f)
 
     calc = source_code.strip().split("\n")[-1].strip()
     if calc.startswith("return "):
         calc = calc[len("return "):]
+    print(calc)
+    calc = derive(calc)
 
     return calc
 
