8060715635c928f38868d1f6d0f33d3d223cb0e0
Nathan <n49874687@gmail.com>
2025-10-05 09:40:26 +0200

    fix: fonction d'affichage des fonctions d'erreur

--- mlkit/linear_model/error_functions.py
+++ mlkit/linear_model/error_functions.py
@@ -12,24 +12,29 @@ class ErrorFunction:
         raise NotImplementedError("Subclasses should implement this method.")
 
 class MeanSquaredError(ErrorFunction):
+    def function(self, y_true:np.ndarray, y_pred:np.ndarray) -> np.ndarray:
+        return (y_true - y_pred) ** 2
     def compute(self, y_true:np.ndarray, y_pred:np.ndarray) -> float:
         return ((y_true - y_pred) ** 2).mean()
 
 class MeanAbsoluteError(ErrorFunction):
+    def function(self, y_true:np.ndarray, y_pred:np.ndarray) -> np.ndarray:
+        return np.abs(y_true - y_pred)
     def compute(self, y_true:np.ndarray, y_pred:np.ndarray) -> float:
         return np.abs(y_true - y_pred).mean()
 
-def plot_function(error_function:ErrorFunction):
-    """Plot the function in blue and its derivative in red"""
-    x = np.linspace(1, 100)
-
-    plt.plot(x, color='blue', label='Function')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('')
+def plot_function(error_function: ErrorFunction, y_true=1.0, y_pred_range=(0, 2), num_points=100):
+    """Plot the error function in blue and its derivative in red."""
+    y_pred = np.linspace(y_pred_range[0], y_pred_range[1], num_points)
+    y_true_arr = np.full_like(y_pred, y_true)
+    errors = error_function.function(y_true_arr, y_pred)
+    print(errors, y_pred)
+    plt.plot(y_pred, errors, color='blue', label='Error Function')
+    plt.xlabel('y_pred')
+    plt.title(f'{error_function.__class__.__name__}')
     plt.legend()
-    plt.show()
     plt.savefig("figures/error_function.png")
+    plt.show()
 
 if __name__ == "__main__":
     a = np.array([1])
@@ -39,4 +44,4 @@ if __name__ == "__main__":
 
     print(MeanSquaredError().compute(a, b))
 
-    plot_function(MeanAbsoluteError)
+    plot_function(MeanSquaredError())
