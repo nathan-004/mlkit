8cd8b6da8ea9dc7d861483f84eba3d15171802a2
Nathan <n49874687@gmail.com>
2025-10-05 09:53:59 +0200

    feat: affichage de la dérivée des fonctions d'erreur

--- mlkit/linear_model/error_functions.py
+++ mlkit/linear_model/error_functions.py
@@ -12,36 +12,45 @@ class ErrorFunction:
         raise NotImplementedError("Subclasses should implement this method.")
 
 class MeanSquaredError(ErrorFunction):
-    def function(self, y_true:np.ndarray, y_pred:np.ndarray) -> np.ndarray:
-        return (y_true - y_pred) ** 2
-    def compute(self, y_true:np.ndarray, y_pred:np.ndarray) -> float:
-        return ((y_true - y_pred) ** 2).mean()
+    def function(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        return ((y_true - y_pred) ** 2) / y_true.shape[0]
 
-class MeanAbsoluteError(ErrorFunction):
-    def function(self, y_true:np.ndarray, y_pred:np.ndarray) -> np.ndarray:
+    def compute(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.mean((y_true - y_pred) ** 2)
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        n = y_true.shape[0]
+        return (2 / n) * (y_pred - y_true)
+
+import numpy as np
+
+class MeanAbsoluteError:
+    def function(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         return np.abs(y_true - y_pred)
-    def compute(self, y_true:np.ndarray, y_pred:np.ndarray) -> float:
-        return np.abs(y_true - y_pred).mean()
+
+    def compute(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.mean(np.abs(y_true - y_pred))
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        return np.where(y_pred > y_true, 1, np.where(y_pred < y_true, -1, 0))
+
 
 def plot_function(error_function: ErrorFunction, y_true=1.0, y_pred_range=(0, 2), num_points=100):
     """Plot the error function in blue and its derivative in red."""
     y_pred = np.linspace(y_pred_range[0], y_pred_range[1], num_points)
     y_true_arr = np.full_like(y_pred, y_true)
     errors = error_function.function(y_true_arr, y_pred)
-    print(errors, y_pred)
+
+    derivatives = error_function.derivative(y_true_arr, y_pred)
+
     plt.plot(y_pred, errors, color='blue', label='Error Function')
+    plt.plot(y_pred, derivatives, color='red', label='Derivative')
     plt.xlabel('y_pred')
+    plt.ylabel('Value')
     plt.title(f'{error_function.__class__.__name__}')
     plt.legend()
     plt.savefig("figures/error_function.png")
     plt.show()
 
 if __name__ == "__main__":
-    a = np.array([1])
-    print(a)
-
-    b = np.array([0])
-
-    print(MeanSquaredError().compute(a, b))
-
-    plot_function(MeanSquaredError())
+    plot_function(MeanAbsoluteError())
